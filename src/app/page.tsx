3ea9f2a4a4c1f38b14a6622bbea4c3e477537336
vishnus111 <vishnusudeesh390@gmail.com>
2024-03-03 00:18:57 +0530

    herosection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "@/components/HeroSection"
 
 
 const Home = () => {
   return (
     <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
-      <h1 className='text=2xl text-center'>The audio god</h1>
+      
+      <HeroSection/>
     </main>
   )
 }
